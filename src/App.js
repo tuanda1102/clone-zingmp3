bc6b7389f0c31ec5e9d483c78ce99c575251aefd
tuanda1102 <phamanhtuan200420@gmail.com>
2022-06-29 00:21:00 +0700

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import '~/assets/css/base.css';
+function App() {
+    return (
+        <div className="App">
+            <div className="zing-app container-fluid"></div>
+        </div>
+    );
+}
+
+export default App;
